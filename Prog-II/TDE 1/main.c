6b99d6b9d39ef1f91b5b73bd603b239e81ebf655
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-09-30 12:18:54 +0000

    up

--- Prog-II/TDE 1/main.c
+++ Prog-II/TDE 1/main.c
@@ -1,14 +1,236 @@
 // TDE 1
+
 // 1
-int main(){
-    int matriz[2][4];
-    for(int i=0;i<2;i++){
-        for(int j=0;j<4;j++){
-            print("Numero: ");
-            scanf("%d",matriz[i][j]);
-            print("[%d]",matriz[i][j]);
-        }
-        print("\n");
-    }
-    return 0;
-}
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int matriz[2][4];
+//     int resultado[2][4];
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("Numero: ");
+//             scanf("%d", &matriz[i][j]);
+//         }
+//     }
+
+//     for(int i = 0; i < 2; i++) {
+//         for(int j = 0; j < 4; j++) {
+//             resultado[i][j] = 2 * matriz[i][j];
+//         }
+//     }
+
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("[%d]", resultado[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// 2
+
+// #include <stdio.h>
+
+// int main() {
+//     int A[3][3], B[3][3], resultado[3][3];
+//     int i, j;
+
+//     printf("matriz A (3x3):\n");
+//     for(i = 0; i < 3; i++) {
+//         for(j = 0; j < 3; j++) {
+//             printf("A[%d][%d]: ", i+1, j+1);
+//             scanf("%d", &A[i][j]);
+//         }
+//     }
+
+//     printf("\nmatriz B (3x3):\n");
+//     for(i = 0; i < 3; i++) {
+//         for(j = 0; j < 3; j++) {
+//             printf("B[%d][%d]: ", i+1, j+1);
+//             scanf("%d", &B[i][j]);
+//         }
+//     }
+
+//     for(i = 0; i < 3; i++) {
+//         for(j = 0; j < 3; j++) {
+//             resultado[i][j] = 3 * A[i][j] - B[i][j];
+//         }
+//     }
+
+//     printf("\nResultado (3 * A - B):\n");
+//     for(i = 0; i < 3; i++) {
+//         for(j = 0; j < 3; j++) {
+//             printf("[%d] ", resultado[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// 3
+// #include <stdio.h>
+
+// int main()
+// {
+//     int matriz[5][2], transposta[2][5];
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             printf("Matriz Original [%d][%d]: ", i + 1, j + 1);
+//             scanf("%d", &matriz[i][j]);
+//             transposta[j][i] = matriz[i][j];
+//         }
+//     }
+//     printf("\nMatriz Original:\n");
+//     for (int i = 0; i < 5; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             printf("[%d] ", matriz[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("\nMatriz Transpota:\n");
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             printf("[%d] ", transposta[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// 4
+// #include <stdio.h>
+
+// int main()
+// {
+//     int matriz[4][2],matrizb[2][4], transposta[2][4], resultado[2][4];
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             printf("Matriz A [%d][%d]: ", i + 1, j + 1);
+//             scanf("%d", &matriz[i][j]);
+//             transposta[j][i] = matriz[i][j];
+//         }
+//     }
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("Matriz B [%d][%d]: ", i + 1, j + 1);
+//             scanf("%d", &matrizb[i][j]);
+//         }
+//     }
+
+//     for(int i = 0; i < 2; i++) {
+//         for(int j = 0; j < 4; j++) {
+//             resultado[i][j] = transposta[i][j] * matrizb[i][j];
+//         }
+//     }
+
+//     printf("\nMatriz A:\n");
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             printf("[%d] ", matriz[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("\nMatriz B:\n");
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("[%d] ", matrizb[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     printf("\nMatriz Resultado a*b:\n");
+//     for (int i = 0; i < 2; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("[%d] ", resultado[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// 5
+// #include <stdio.h>
+
+// int main()
+// {
+//     int matriz[3][3] = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}};
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             if (j > i)
+//             {
+//                 printf("%d ", matriz[i][j]);
+//             }
+//             else
+//             {
+//                 printf("* ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// 6
+// #include <stdio.h>
+
+// int main()
+// {
+//     int matriz[4][4] = {
+//         {1, 2, 3, 4},
+//         {5, 6, 7, 8},
+//         {9, 10, 11, 12},
+//         {13, 14, 15, 16}};
+//     int soma=0;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             if (j <= i)
+//             {
+//                 printf("%d ", matriz[i][j]);
+//                 soma+=matriz[i][j];
+//             }
+//             else
+//             {
+//                 printf("* ");
+//             }
+//         }
+//         printf("\n");
+//     }
+//     printf("A soma de todos os valores do triângulo inferior da diagonal principal, incluindo a diagonal principal de uma matriz 4x4 é: %d\n",soma);
+//     return 0;
+// }
+
+// 7
