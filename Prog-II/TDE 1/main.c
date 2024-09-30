e403d6e9484acaa14c9a4f6615aec866e36ab1cd
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-09-30 13:31:58 +0000

    up

--- Prog-II/TDE 1/main.c
+++ Prog-II/TDE 1/main.c
@@ -261,34 +261,36 @@
 // }
 
 // 8
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    float matriz[4][4] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}};
-        //  {0,0, 0,1, 0,2, 0,3},
-        // {1,0, 1,1, 1,2, 1,3},
-        // {2,0, 2,1, 2,2, 2,3},
-        // {3,0, 3,1, 3,2, 3,3}};
-    float parcial=0;
-    int count=0;
-    float media=0;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (i + j >=4)
-            {
-                parcial=parcial+matriz[i][j];
-                count++;
-            }
-        }
-    }
-    media=parcial/count;
-    printf("A media é %.2f.\n", media);
-    return 0;
-}
+// int main()
+// {
+//     float matriz[4][4] = {
+//         {1, 2, 3, 4},
+//         {5, 6, 7, 8},
+//         {9, 10, 11, 12},
+//         {13, 14, 15, 16}};
+//         //  {0,0, 0,1, 0,2, 0,3},
+//         // {1,0, 1,1, 1,2, 1,3},
+//         // {2,0, 2,1, 2,2, 2,3},
+//         // {3,0, 3,1, 3,2, 3,3}};
+//     float parcial=0;
+//     int count=0;
+//     float media=0;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             if (i + j >=4)
+//             {
+//                 parcial=parcial+matriz[i][j];
+//                 count++;
+//             }
+//         }
+//     }
+//     media=parcial/count;
+//     printf("A media é %.2f.\n", media);
+//     return 0;
+// }
+
+// 9
