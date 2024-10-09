facc7944c44b929dfb189e17649114a7ff58bbd5
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-10-09 12:05:55 +0000

    up

--- Prog-II/C com B.D/main.c
+++ Prog-II/C com B.D/main.c
@@ -45,13 +45,58 @@ void listar_nomes() {
     fclose(arquivo);
 }
 
+void deletar_nome() {
+    FILE *arquivo = fopen(NOME_ARQUIVO, "r");
+    if (arquivo == NULL) {
+        printf("Nenhum nome cadastrado ainda.\n");
+        return;
+    }
+
+    char nome[MAX_NOME];
+    char nomes[100][MAX_NOME]; // Array para armazenar nomes
+    int contador = 0;
+
+    // Lê todos os nomes do arquivo
+    while (fgets(nome, MAX_NOME, arquivo) != NULL) {
+        nome[strcspn(nome, "\n")] = 0; // Remove o caractere de nova linha
+        strcpy(nomes[contador++], nome);
+    }
+    fclose(arquivo);
+
+    // Exibe os nomes cadastrados para o usuário
+    printf("Nomes cadastrados:\n");
+    for (int i = 0; i < contador; i++) {
+        printf("%d. %s\n", i + 1, nomes[i]);
+    }
+
+    int opcao;
+    printf("Escolha o número do nome para deletar (ou 0 para cancelar): ");
+    scanf("%d", &opcao);
+    getchar(); // Limpa o buffer do stdin
+
+    if (opcao > 0 && opcao <= contador) {
+        // Reescreve o arquivo sem o nome selecionado
+        arquivo = fopen(NOME_ARQUIVO, "w");
+        for (int i = 0; i < contador; i++) {
+            if (i != opcao - 1) { // Ignora o nome a ser deletado
+                fprintf(arquivo, "%s\n", nomes[i]);
+            }
+        }
+        fclose(arquivo);
+        printf("Nome deletado com sucesso!\n");
+    } else if (opcao != 0) {
+        printf("Opção inválida! Nenhum nome deletado.\n");
+    }
+}
+
 int main() {
     int opcao;
 
     do {
         printf("\n1. Cadastrar Nome\n");
         printf("2. Listar Nomes\n");
-        printf("3. Sair\n");
+        printf("3. Deletar Nome\n");
+        printf("4. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
         getchar(); // Limpa o buffer do stdin
@@ -64,12 +109,15 @@ int main() {
                 listar_nomes();
                 break;
             case 3:
+                deletar_nome();
+                break;
+            case 4:
                 printf("Saindo do programa...\n");
                 break;
             default:
                 printf("Opção inválida! Tente novamente.\n");
         }
-    } while (opcao != 3);
+    } while (opcao != 4);
 
     return 0;
 }
