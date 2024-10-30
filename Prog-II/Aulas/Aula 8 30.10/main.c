7be9799d86f6753cea04344606181ed8847fd94b
marcelobfb <145417804+marcelobfb@users.noreply.github.com>
2024-10-30 13:29:30 +0000

    up

--- Prog-II/Aulas/Aula 8 30.10/main.c
+++ Prog-II/Aulas/Aula 8 30.10/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // struct Aluno{
 //     char nome[40];
@@ -60,31 +62,52 @@
 struct alunos
 {
     char nome[40];
-    float nota[3];
+    float notas[3];
 };
 typedef struct alunos aluno;
-float maiorNota(float nota[]);
+void imprimirNomeMaiorNota(aluno *alunos, int qtdAlunos);
 
 int main()
 {
-    aluno alu;
-    printf("Nome: ");
-    fgets(alu.nome,sizeof(alu.nome),stdin);
-    for (int i = 0; i < 3; i++)
+    int qtd;
+    aluno *alunos;
+
+    printf("qual a qtd de alunos? ");
+    scanf("%d",&qtd);
+
+    alunos=(aluno*) malloc(qtd*sizeof(aluno));
+    getchar();
+    for (int i = 0; i < qtd; i++)
     {
-        printf("nota: ");
-        scanf("%f",&alu.nota[i]);
+        printf("Nome: ");
+        fgets(alunos[i].nome,sizeof(alunos[i].nome),stdin);
+        for (int j = 0; j < 3; j++)
+        {
+            printf("Nota: ");
+            scanf("%f",&alunos[i].notas[j]);
+        }
+        getchar();
     }
-    printf("A maior nota é %.2f\n",maiorNota(alu.nota));
+    
+    imprimirNomeMaiorNota(alunos,qtd);
+
     return 0;
 }
-float maiorNota(float nota[]){
-    float mNota=0;
-    for (int i = 0; i < 3; i++)
+void imprimirNomeMaiorNota(aluno *alunos, int qtdAlunos){
+    float maiorNota=0;
+    int indexAluno=0;
+
+    for (int i = 0; i < qtdAlunos; i++)
     {
-        if(nota[i]>mNota){
-            mNota=nota[i];
+        for (int j = 0; j < 3; j++)
+        {
+            if(alunos[i].notas[j]>maiorNota){
+                maiorNota=alunos[i].notas[j];
+                indexAluno=i;
+            }
         }
+        
     }
-    return mNota;
+    
+printf("nome da maior nota: %s",alunos[indexAluno].nome);
 }
